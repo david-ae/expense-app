ac8c60b3705546a138d3efcf60b36185a59b27c6
david-ae <davidedet47@gmail.com>
2023-10-09 20:22:02 +0100

    fake database added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,32 @@
-import {Controller, Get} from "@nestjs/common";
+import { Put } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('report/income')
-export class AppController{
+@Controller('report/:type')
+export class AppController {
   @Get()
-  getAllIncomeReports(){
+  getAllIncomeReports() {
     return [];
+  }
+
+  @Get(':id')
+  getIncomeReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
