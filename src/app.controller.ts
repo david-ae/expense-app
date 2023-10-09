3d1534138832c60c10eb3da0abc50586bf16130c
David Edet <davidedet47@gmail.com>
2023-10-09 18:20:57 +0100

    controller-expense-app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from "@nestjs/common";
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+@Controller('report/income')
+export class AppController{
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllIncomeReports(){
+    return [];
   }
 }
