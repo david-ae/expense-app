da04854e3fdffb1b21c3ce6891d43d169e5ca100
david-ae <davidedet47@gmail.com>
2023-10-12 20:40:18 +0100

    Corrected Validation & Response Formatting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,52 +1,73 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, ParseIntPipe, ParseUUIDPipe, ParseEnumPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  ParseEnumPipe,
+} from '@nestjs/common';
 import { ReportType, data } from './data';
 
 import { AppService } from './app.service';
-import { CreateReportDto, UpdateReportDto } from './dtos/report.dto';
+import {
+  CreateReportDto,
+  ReportResponseDto,
+  UpdateReportDto,
+} from './dtos/report.dto';
 
 @Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService){}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getAllIncomeReports(
-    @Param('type', new ParseEnumPipe(ReportType)) type: string
-    ) {
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+  ): ReportResponseDto[] {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
     return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
   getIncomeReportById(
     @Param('type', new ParseEnumPipe(ReportType)) type: string,
-     @Param('id', ParseUUIDPipe) id: string) {
-      console.log(id, typeof(id));
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    @Param('id', ParseUUIDPipe) id: string,
+  ): ReportResponseDto {
+    console.log(id, typeof id);
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
     return this.appService.getReportById(reportType, id);
   }
 
   @Post()
   createReport(
-    @Param('type', new ParseEnumPipe(ReportType)) type: string, 
-    @Body() {amount, source}: CreateReportDto) {
-      const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
-      return this.appService.createReport(reportType, {amount, source})
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Body() { amount, source }: CreateReportDto,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, { amount, source });
   }
 
   @Put(':id')
   updateReport(
-    @Param('type', new ParseEnumPipe(ReportType)) type: string, @Param('id', ParseUUIDPipe)id: string, 
-    @Body() body: UpdateReportDto
-  ) {
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdateReportDto,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
     return this.appService.updateReport(reportType, id, body);
   }
-  
+
   @HttpCode(204)
   @Delete(':id')
-  deleteReport(
-    @Param('id', ParseIntPipe) id: string
-    ) {
+  deleteReport(@Param('id', ParseIntPipe) id: string) {
     this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,55 +1,75 @@
-import { Injectable } from "@nestjs/common";
-import { ReportType, data } from "./data";
-import {v4 as uuid} from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { ReportType, data } from './data';
+import { v4 as uuid } from 'uuid';
+import { ReportResponseDto } from './dtos/report.dto';
 
-interface Report{ amount: number, source: string}
-interface UpdateReport{ amount?: number, source?: string}
+interface Report {
+  amount: number;
+  source: string;
+}
+interface UpdateReport {
+  amount?: number;
+  source?: string;
+}
 
 @Injectable()
-export class AppService{
-  getAllReports(type: ReportType){
-    return data.report.filter((report) => report.type === type);
-  }
-
-  getReportById(type: ReportType, id: string){
+export class AppService {
+  getAllReports(type: ReportType): ReportResponseDto[] {
     return data.report
-    .filter((report) => report.type === type)
-    .find(report => report.id === id)
+      .filter((report) => report.type === type)
+      .map((report) => new ReportResponseDto(report));
   }
 
-  createReport(type: ReportType, {amount, source}: Report){
+  getReportById(type: ReportType, id: string): ReportResponseDto {
+    const report = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!report) return;
+
+    return new ReportResponseDto(report);
+  }
+
+  createReport(
+    type: ReportType,
+    { amount, source }: Report,
+  ): ReportResponseDto {
     const newReport = {
       id: uuid(),
       source,
       amount,
       created_at: new Date(),
       updated_at: new Date(),
-      type
+      type,
     };
     data.report.push(newReport);
-    return newReport;
+    return new ReportResponseDto(newReport);
   }
 
-  updateReport(type: ReportType, id: string, body: UpdateReport){
-    const report = data.report.find(r => r.id === id);
+  updateReport(
+    type: ReportType,
+    id: string,
+    body: UpdateReport,
+  ): ReportResponseDto {
+    const report = data.report.find((r) => r.id === id);
 
-    if(!report) return;
+    if (!report) return;
 
     const reportIndex = data.report.findIndex((r) => r.id === report.id);
-    
+
     data.report[reportIndex] = {
       ...data.report[reportIndex],
       ...body,
-      updated_at: new Date()
+      updated_at: new Date(),
     };
 
-    return data.report[reportIndex];
+    return new ReportResponseDto(data.report[reportIndex]);
   }
 
-  deleteReport(id:string){
-    const reportIndex = data.report.findIndex(report => report.id === id);
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
 
-    if(reportIndex === -1){
+    if (reportIndex === -1) {
       return;
     }
 
